9ff5a45395743785e12ac49f5e53291c842c659b
Joseph Kunzler <jkunzler@inductor.ai>
2024-02-06 16:34:11 -0800

    vzxc

--- infrastructure/publish_server/__main__.py
+++ infrastructure/publish_server/__main__.py
@@ -4,4 +4,14 @@ import sys
 print(sys.path)
 
 
-from infrastructure import config
+import sys  # pylint: disable=wrong-import-order
+import pathlib  # pylint: disable=wrong-import-order
+# Add parent directory to path in order to import config.
+# This is necessary because the GitHub Actions workflow runs the script
+# from the infrastructure/publish_server directory, and is unable to
+# import from the parent directory (infrastructure).
+current_dir = pathlib.Path(__file__).resolve().parent
+parent_dir = current_dir.parent
+sys.path.append(str(parent_dir))
+
+import config  # pylint: disable=wrong-import-order,wrong-import-position
